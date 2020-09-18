23260025dea4e3e3c126066add83436d6fb2308a
Bread traversy <mar@breadtraversy.com>
2020-09-18 22:01:33 +0200

    mvvm viewmodel livedata

---
+++ app/src/main/java/com/example/architectureexample/Note.java
@@ -0,0 +1,39 @@
+package com.example.architectureexample;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "note_table")
+public class Note {
+    @PrimaryKey (autoGenerate = true)
+    private int id;
+    private String title;
+    private String description;
+    private int priority;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Note(String title, String description,int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
